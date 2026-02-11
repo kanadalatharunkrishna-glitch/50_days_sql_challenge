b7aea335c578380d71bbe99c49df9de39f8540e2
Tharun krishna <kanadalatharunkrishna@gmail.com>
2026-02-11 19:12:10 +0530

    Add files via upload

---
+++ sql challenge Day -10.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary NUMERIC(10, 2)
+);
+
+INSERT INTO employees (EmployeeID, FirstName, LastName, Department, Salary) VALUES
+(1, 'John', 'Doe', 'Finance', 75000.00),
+(2, 'Jane', 'Smith', 'HR', 60000.00),
+(3, 'Michael', 'Johnson', 'IT', 45000.00),
+(4, 'Emily', 'Brown', 'Marketing', 55000.00),
+(5, 'David', 'Williams', 'Finance', 80000.00),
+(6, 'Sarah', 'Jones', 'HR', 48000.00),
+(7, 'Chris', 'Taylor', 'IT', 72000.00),
+(8, 'Jessica', 'Wilson', 'Marketing', 49000.00);
+
+select * from employees;
+
+
+Write a SQL query to classify employees into three categories based on their salary:
+
+"High" - Salary greater than $70,000
+"Medium" - Salary between $50,000 and $70,000 (inclusive)
+"Low" - Salary less than $50,000
+
+Your query should return the EmployeeID, FirstName, LastName, Department, Salary, and 
+a new column SalaryCategory indicating the category to which each employee belongs.
+
+select *, 
+   case 
+   when salary > 70000 Then 'Higher'
+   when salary between 50000 and 70000 then 'Average'
+   else 'Low'
+   end as SalaryCategory
+ from employees;
